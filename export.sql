4c63cc5e9f390fab9ef55daaaf7284a6bdab392f
ANykyfor <artproger@outlook.com>
2025-05-19 23:57:13 +0300

    � Додано export.sql для вивантаження таблиць у CSV

---
+++ export.sql
@@ -0,0 +1,9 @@
+COPY Riders TO 'riders_export.csv' DELIMITER ',' CSV HEADER;
+
+COPY Trips TO 'trips_export.csv' DELIMITER ',' CSV HEADER;
+
+COPY Events TO 'events_export.csv' DELIMITER ',' CSV HEADER;
+
+COPY Motorcycles TO 'motorcycles_export.csv' DELIMITER ',' CSV HEADER;
+
+COPY Payments TO 'payments_export.csv' DELIMITER ',' CSV HEADER;
